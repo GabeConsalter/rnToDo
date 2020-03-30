3ed72ea70f6cf2ebf7b3735fb944b0704d685345
Gabriel Consalter <consaltergabriel@gmail.com>
2020-03-30 16:24:48 -0300

    Added new eslint rules; Fix lint problems to code pattern

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-const App = () => {
-	return (
-		<View>
-			<Text>Hello ToDo</Text>
-		</View>
-	);
-}
+const App = () => (
+	<View>
+		<Text>Hello ToDo</Text>
+	</View>
+);
 
 export default App;
