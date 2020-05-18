1c6d6269b6697bc12b87f89c1821435e88cab822
Gabriel Consalter <consaltergabriel@gmail.com>
2020-05-18 16:54:39 -0300

    test: :white_check_mark: Review test dependencies; Implemented initial tests with Testing Library

---
+++ __tests__/InputComponent.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Input from '../src/components/Input.js';
+import { fireEvent, render } from '@testing-library/react-native';
+
+describe('Input component', () => {
+	const { getByTestId } = render(<Input />),
+		textInput = getByTestId('textInput');
+
+	it('should start empty', () => {
+		expect(textInput.props.value).toBe('');
+	});
+
+	it('should typed "testing"', () => {
+		fireEvent.changeText(textInput, 'testing');
+
+		expect(textInput.props.value).toBe('testing');
+	});
+});
