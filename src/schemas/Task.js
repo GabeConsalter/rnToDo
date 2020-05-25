afc7c749ad3e5794629ab1e6a2a699606b610f0c
Gabriel Consalter <consaltergabriel@gmail.com>
2020-05-25 18:38:27 -0300

    chore: Updated Task schema
    Changed date props to date string type for now. Added save and get methods
    
    \

--- src/schemas/Task.js
+++ src/schemas/Task.js
@@ -1,4 +1,6 @@
 import Utils from '../Utils';
+import getRealm from '../realm';
+import moment from 'moment';
 
 /**
  * Task
@@ -14,15 +16,42 @@ class Task {
 	 * @prop {string} guid=Utils.guid() - The guid to identify a task
 	 * @prop {string} text - The text describing what have to do
 	 * @prop {bool} done=false - The done status indicating if task is done or not
-	 * @prop {Date} createdAt=new Date() - The moment that task was created
-	 * @prop {Date} [updatedAt] - The moment that task was updated
+	 * @prop {string} createdAt=new Date() - The moment that task was created
+	 * @prop {string} [updatedAt] - The moment that task was updated
 	 */
 	constructor(task) {
 		this.guid = task.guid ? task.guid : Utils.guid();
 		this.text = task.text;
-		this.done = task.done = false;
-		this.createdAt = new Date();
-		this.updatedAt = task.updatedAt;
+		this.done = task.done ? task.done : false;
+		this.createdAt = task.createdAt ? task.createdAt : moment().toISOString();
+		this.updatedAt = moment(task.updatedAt).toISOString();
+	}
+
+	/**
+	 * Save a task
+	 *
+	 * @author Gabriel Consalter
+	 * @since 1.0.0
+	 */
+	async save() {
+		const realm = await getRealm();
+
+		realm.write(() => realm.create('Task', this));
+	}
+
+	/**
+	 * Get a task by guid
+	 *
+	 * @param {string} guid
+	 *
+	 * @author Gabriel Consalter
+	 * @since 1.0.0
+	 */
+	static async get(guid) {
+		const realm = await getRealm(),
+			task = realm.objectForPrimaryKey('Task', guid);
+
+		return task ? new Task({ ... JSON.parse(JSON.stringify(task)) }) : null;
 	}
 }
 
@@ -36,8 +65,8 @@ Task.schema = {
 		},
 		text: 'string',
 		done: 'bool',
-		createdAt: 'date',
-		updatedAt: 'date?'
+		createdAt: 'string',
+		updatedAt: 'string?'
 	}
 };
 
