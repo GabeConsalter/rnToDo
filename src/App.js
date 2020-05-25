894f65386b8500200e81b044c114787f35b208b7
Gabriel Consalter <consaltergabriel@gmail.com>
2020-05-25 15:51:08 -0300

    feat: :sparkles: Added List of tasks

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
+import { List } from './components';
+import { Task } from './schemas';
+import Input from './components/Input';
 
-const App = () => (
-	<View style={styles.container} />
-);
+const App = () => {
+	const [tasks, setTasks] = useState([]);
+
+	function addTask(text) {
+		const task = new Task({ text });
+
+		setTasks([...tasks, task]);
+	}
+
+	return (
+		<View style={styles.container}>
+			<List
+				tasks={tasks}
+				onTaskPress={task => console.log(task)}
+			/>
+			<Input
+				onSendTask={text => addTask(text)}
+			/>
+		</View>
+	);
+};
 
 const styles = StyleSheet.create({
 	container: {
 		backgroundColor: '#273251',
 		flex: 1,
-		paddingHorizontal: 16
+		padding: 16
 	}
 });
 
