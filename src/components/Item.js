c47fb5eae35938118a81dc9cb2a362124481c0b9
Gabriel Consalter <consaltergabriel@gmail.com>
2020-05-25 16:11:30 -0300

    chore: Added done management and their styles

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,46 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-const Item = props => (
-	<TouchableOpacity style={styles.container}>
-		<View>
-			<Text style={styles.text}>{props.item.text}</Text>
-		</View>
-		<TouchableOpacity style={styles.btnStatus} />
-	</TouchableOpacity>
-);
+const Item = props => {
+	const [done, setDone] = useState(props.item.done),
+		styles = StyleSheet.create({
+			btnStatus: {
+				backgroundColor: done ? '#348' : 'transparent',
+				borderColor: '#348',
+				borderRadius: 16,
+				borderWidth: 1,
+				height: 32,
+				width: 32
+			},
+			container: {
+				alignItems: 'center',
+				backgroundColor: `#000000${done ? '33' : '55'}`,
+				borderRadius: 7,
+				flexDirection: 'row',
+				justifyContent: 'space-between',
+				marginBottom: 8,
+				paddingHorizontal: 8,
+				paddingVertical: 16,
+				width: '100%'
+			},
+			text: {
+				color: `#FFFFFF${done ? '33' : 'FF'}`
+			}
+		});
+
+	return (
+		<TouchableOpacity style={styles.container}>
+			<View>
+				<Text style={styles.text}>{props.item.text}</Text>
+			</View>
+			<TouchableOpacity
+				testID="btnStatus"
+				onPress={() => setDone(!done)}
+				style={styles.btnStatus}
+			/>
+		</TouchableOpacity>
+	);
+};
 
 Item.propTypes = {
 	item: PropTypes.object.isRequired
+
 };
 
 Item.displayName = 'Item';
-
-const styles = StyleSheet.create({
-	btnStatus: {
-		borderColor: '#348',
-		borderRadius: 16,
-		borderWidth: 1,
-		height: 32,
-		width: 32
-	},
-
-	container: {
-		alignItems: 'center',
-		backgroundColor: '#00000055',
-		borderRadius: 7,
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-		marginBottom: 8,
-		paddingHorizontal: 8,
-		paddingVertical: 16,
-		width: '100%'
-	},
-
-	text: {
-		color: 'white'
-	}
-});
 
 export default Item;
