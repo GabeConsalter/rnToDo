291e92a13261fab745f01518dac3e50178b09e40
Gabriel Consalter <consaltergabriel@gmail.com>
2020-07-02 08:36:14 -0300

    feat: :sparkles: List order and group
    List grouped by undone tasks first and done tasks after; Added updated date sort in each group

--- src/components/List.js
+++ src/components/List.js
@@ -1,24 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import moment from 'moment';
 import PropTypes from 'prop-types';
 import { FlatList, StyleSheet } from 'react-native';
 import { Item } from '.';
+import { Task } from '../schemas';
 
-const List = props => (
-	<FlatList
-		style={styles.container}
-		data={props.tasks}
-		renderItem={({ item: task }) => (
-			<Item
-				text={task.text}
-				done={task.done}
-				onSetDone={done => {
-					task.done = done;
-					task.update();
-				}}
-			/>
-		)}
-	/>
-);
+const List = props => {
+	const [tasks, setTasks] = useState(props.tasks);
+
+	useEffect(() => {
+		setTasks(order(props.tasks));
+	}, [props.tasks]);
+
+	function order(tasks) {
+		const undoneTasks = tasks.filter(task => !task.done);
+		undoneTasks.sort((a, b) => a.updatedAt < b.updatedAt);
+
+		const doneTasks = tasks.filter(task => task.done);
+		doneTasks.sort((a, b) => a.updatedAt < b.updatedAt);
+
+		return [...undoneTasks, ...doneTasks];
+	}
+
+	return (
+		<FlatList
+			testID="list"
+			style={styles.container}
+			data={tasks}
+			renderItem={({ item: task }) => (
+				<Item
+					text={`${task.text}`}
+					done={task.done}
+					onSetDone={async done => {
+						task.done = done;
+						task.update();
+
+						console.log(task);
+
+						setTasks(order(await Task.getAll()));
+					}}
+				/>
+			)}
+		/>
+	);
+};
 
 List.propTypes = {
 	tasks: PropTypes.array
